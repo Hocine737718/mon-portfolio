b1bad5b90e60c9b179249f7d13d21d63f64d5c5b
hocine <hocine737718@gmail.com>
2023-12-25 23:01:28 +0100

    Projects Commit

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,23 @@
 (function ($) {
-    "use strict"; 
+    "use strict";
+    // SMOOTHSCROLL
+    $(function() {
+        $('.nav-link, .custom-btn-link').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top - 49
+            }, 1000);
+            event.preventDefault();
+        });
+    }); 
     // HEADER
     $(".navbar").headroom(); 
+    // PROJECT CAROUSEL
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop:true,
+        margin:10,
+        nav:true
+    });
 })(jQuery);
   
